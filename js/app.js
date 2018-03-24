9a800270fd31eaadf3d469a5d1bf146333ac7ef3
Antice <ken.r.e.madsen@gmail.com>
2018-03-24 14:58:06 +0100

    Added some simple combat logic.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
-// setting up some data
+// Data:
 
 
+// Utility classes:
 
-
-// Simple die roller
-
+//d-10
 var d10 = function(){
   return Math.floor(Math.random()* 10 + 1);
 }
@@ -17,5 +16,54 @@ var do10 = function(num){
 
 // Tile generation functions
 var roomtype = function(){
-  return roomtypes[d10()];
+  return roomTypes[d10()];
+}
+
+
+
+
+
+// game engine
+
+// battle:
+var enemyHP = 100;
+var enemyDefenceVal = 2;
+var enemyWeaponStr = 2;
+var playerHP = 100;
+var playerDefenceVal = 4;
+var playerWeaponStr = 4;
+
+
+// attacking
+var playerAttack = function(){
+  //roll to hit
+  if (d10() <= enemyDefenceVal - playerWeaponStr){
+    console.log('Miss!');
+  }
+  else{
+    console.log('You hit him')
+    // how much damage?
+    var hit = d10() + playerWeaponStr - enemyDefenceVal;
+    enemyHP = enemyHP - hit;
+    }
+  // is it dead yet?
+  if (enemyHP < 1){
+    console.log('The enemy has died.')
+  }
+}
+
+var enemyAttack = function(){
+  if (d10() <= playerDefenceVal - enemyWeaponStr){
+    console.log('Miss!');
+  }
+  else{
+    console.log('You got hit')
+    // how much damage?
+    var hit = d10() + enemyWeaponStr - playerDefenceVal;
+    playerHP = playerHP - hit;
+    }
+  // dead yet?
+  if (playerHP < 1){
+    console.log('Oh noes! You died!!!')
+  }
 }
