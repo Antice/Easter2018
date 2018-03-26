c479dca5a23de7239153670bc634f9497be700ec
Antice <ken.r.e.madsen@gmail.com>
2018-03-26 21:56:51 +0200

    Refactored a loop, and added space for item/ encounter tables and stuff.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 
+// Some global variables needed to progress the game. (sorry. I know this is a bad way to do shit)
 
 // character sheet:
 var pName = '';
@@ -11,6 +12,27 @@ var pArm = ['',0];
 var inv = ['-','-','-','-','-','-','-','-','-'];
 var Torchtime = 0;
 var PlayerIsAlive = false;
+
+// Monster sheet:
+var encName = '';
+var encAtk = 0;     //  not always a weapon....
+var encDef = 0;     // not always armour either
+var encHp = 0;
+
+
+/* item tables:      use a 2d array?  [itemname,type,value]
+--- doing this should remove the need to have item name anywhere else, just refer to the item with item[x],
+ and fetch the corresponding value from the referenced array dimension. */
+
+
+/* Loot tables:     another 2d array? [itemname,type,value].
+same thing here, the loot should be based on the enemy, so one enemy should drop x type of loot only.
+*/
+
+/* encounter tables. format: encounter[x] where x is a second array containing the enemy's stats in the format: encName,encAtk,encDef and encHp.
+We can mess with randomizing the enemy stats a bit later on, by adding a weight variable rather than a direct value */
+
+
 
 // setting up the character sheet at game start:
 function newGame(){
@@ -31,47 +53,26 @@ function newGame(){
 
 
 
-// Bladocument.getElementById('textChange').innerHTML = retVal;nk enemy encounter sheet
-var encName = '';
-var encAtk = 0;
-var encDef = 0;
-var encHp = 0;
 
 
+// some other global stuff we need. should try to avoid doing this as much as possible tho.
 
-// Some global variables needed to progress the game. (sorry. I know this is a bad way to do shit)
 var isEncounter = true;
 
-
-// Utility classes:
-
-//d-10
-var d10 = function(){
-  return Math.floor(Math.random()* 10 + 1);
-}
-
-// Tile generation functions
+/* some temporary/under testing stuff */
+// Tile generation functions that I may or may not have time to implement.
 var roomtype = function(){
   return roomTypes[d10()];
 }
 
-// battle:
-var enemyHP = 100;
-var enemyDefenceVal = 2;
-var enemyWeaponStr = 2;
-var playerHP = 100;
-var playerDefenceVal = 4;
-var playerWeaponStr = 4;
+// Game engine stuff
 
-// Game engine
+//Roll the dice.
+var d10 = function(){
+  return Math.floor(Math.random()* 10 + 1);
+}
 
-//Resolving Combat
-
-// Enemy stats
-
-
-
-// The infamous lord of random
+// Does it hit?
 var attack = function(attack,defence){
   if (attack + d10() > defence + d10()){
     return true;
@@ -86,7 +87,7 @@ var attack = function(attack,defence){
 var hit = function(attack,defence,actor1,actor2){
   var damage = attack + d10() - defence;
   if (damage <= 1){
-    console.log( actor1 +'\'s strike barely scratches the' + ' ' + actor2 + ', ' + 'dealing' + ' ' + damage + ' ' + 'HP worth of damage');
+    console.log( actor1 +'\'s strike barely scratches the' + ' ' + actor2 + ', ' + 'dealing' + ' ' + damage + ' ' + 'HP worth of damage');   // <-- This is such a silly way to do this.
     return damage;
   }
   else if (damage > 1 && damage <= 3) {
@@ -103,29 +104,30 @@ var hit = function(attack,defence,actor1,actor2){
   }
 }
 
-// player Inputs:
+
+// player initiated gameturn;
+// because I don't knwo a better way within js.
 
 function PlayerInput(action,option){
   if (PlayerIsAlive == false){
-    console.log('Dead people can\'t play ball <br> Why not start a new game?')
+    console.log('Dead people can\'t play ball <br> Why not start a new game?');
   }
   else if (action == 'attack'){
     if (isEncounter == true){
-      console.log('You attack')
-      encHp = encHp - hit(pAtk,encDef,pName,encName)
+      encHp =- hit(pAtk,encDef,pName,encName);
       if (encHp <= 0){
         console.log('The enemy has died');
         isEncounter = false;
         return;
       }
-      pHp = pHp - hit(encAtk,pDef,encName,pName)
+      pHp =- hit(encAtk,pDef,encName,pName);
         if (pHp <= 0){
           console.log('You have died');
           PlayerIsAlive = false;
         }
       }
     else{
-      console.log('Nothing there to kill')
+      console.log('Nothing there to kill');
     }
   }
   else if (action == 'use'){
@@ -133,21 +135,21 @@ function PlayerInput(action,option){
     // Let's use the item stored in inventory slot option
   }
   else if (action == 'move'){
-    if (option == 'N'){
-      console.log('You walked north')
+    switch (option) {
+      case 'N':
+        console.log('You walked north');
+      break;
+      case 'S':
+        console.log('You walked south');
+      break;
+      case 'E':
+        console.log('You walked east');
+      break;
+      case 'W':
+        console.log('You walked west');
+      break;
+      default:
+      console.log('This should never happen!!!')
     }
-    else if (option == 'E'){
-      console.log('You walked east')
-    }
-    else if (option == 'S'){
-      console.log('You walked south')
-    }
-    else if (option == 'W'){
-      console.log('You walked west')
-    }
-    else {
-      console.log('This should not happen')
-    }
-
   }
 }
