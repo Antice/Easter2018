7b6a528c58985990431e7db751a05bbc156d9e12
Antice <ken.r.e.madsen@gmail.com>
2018-04-01 23:51:10 +0200

    Defined a map tile. Search now shows what is in your tile. you can pick up and drop items. picked up items now work. (had to revamp the invenotry code to something more robust for this). Added a lot more game init stuff. Lots of minor changes and fixes. EOD

--- js/app.js
+++ js/app.js
@@ -13,6 +13,7 @@ var player = {
 }
 // item table
 var itemTable = {
+    "index":["Potion","Torch","Mysterious note","Rusty Knife","Broken Sword","cracked club","Worn Rags"],
     "Potion":{
       "effect":["HPup",50],
       "fText": "You drink the potion, You feel healthier"
@@ -73,8 +74,17 @@ var game = {
   "encounter": false,
   "enemy":"",
   "enemyHp":0,
-  "items":[""]
-// add tiles and stuff later
+  "maptile":{"0":{"0":["dark cave","Rat","Potion"]}},
+  "x":0,
+  "y":0,
+  "logHistory":['Welcome']
+}
+var ground = game.maptile[game.x][game.y];    // let's make dealing with the map a bit easier on ourselves.
+// add tiletyupes and more stuff later with some fancy map population function. encounters and loot are randomized elsewhere for now.
+function refInv(){
+  for (var i = 0; i < player.inv.length ; i++) {
+    document.getElementById('Slot'+ i).innerHTML = player.inv[i];
+  }
 }
 // preparing a new game. stats has to be reset to start values annyway.
 function newGame(){
@@ -92,19 +102,34 @@ function newGame(){
   for (var i = 0; i < player.inv.length ; i++) {
     document.getElementById('Slot'+ i).innerHTML = player.inv[i];
   }
+  //resetting the map:
+ for (var x = 0; x < game.maptile.length; x++) {
+   for (var y = 0; y < game.maptile[x].length; y++) {
+     game.maptile[x][y] = [];
+   }
+ }
+ game.maptile[0][0] = ["dark cave","Rat","Potion"];
+ game.x = 0;
+ game.y = 0;
+ game.encounter = true;
+ game.enemy = "Rat";
+ game.enemyHp = 10;
+ game.logHistory = [];
+ addToLog('You startle awake, just as a rat is about to bite down on you.');
+ addToLog('Fumbling around, your hand lands on the handle of a knife.');
+ addToLog('picking the knife up you prepare to defend yourself!');
 }
+
 // log output to screen:
-// got to keep tabs on the old stuff, so a variable must be set.
-var logHistory = ['Welcome'];
 //adding new log entries:
 function addToLog(newLogEntry){
   var logString = '';
-  logHistory.push( '<p>' + newLogEntry + '</p>');
-  if(logHistory.length > 20){
-  logHistory.shift();
+  game.logHistory.push( '<p>' + newLogEntry + '</p>');
+  if(game.logHistory.length > 20){
+  game.logHistory.shift();
   }
-  for(i = 0; logHistory.length > i; i++){
-  logString = logString + logHistory[i];
+  for(i = 0; game.logHistory.length > i; i++){
+  logString = logString + game.logHistory[i];
   }
   document.getElementById('log').innerHTML = logString;
 }
@@ -143,6 +168,7 @@ var combatRound = function(){
     if (game.enemyHp < 0) {
       addToLog('The enemy has died');
       game.encounter = false;
+      ground[1] = 'Dead' + ' ' + game.enemy;
     }
     else {
       addToLog('The enemy has' + ' ' + game.enemyHp + ' ' + 'HP left.');
@@ -152,7 +178,10 @@ var combatRound = function(){
     addToLog('You miss');
   }
 // enemy turn:
-  if (attack(encTable[game.enemy].encAtk, (player.pDef + itemTable[player.pArm].effect[1]))){
+  if (game.encounter == false) {
+    return;
+  }
+  else if (attack(encTable[game.enemy].encAtk, (player.pDef + itemTable[player.pArm].effect[1]))){
     addToLog('Enemy attack (hit)');
     player.pHp = player.pHp - (encTable[game.enemy].encAtk + d10() - (player.pDef + itemTable[player.pArm].effect[1]));
     addToLog('you have' + ' ' + player.pHp + ' ' + 'HP left.');
@@ -182,31 +211,37 @@ var runAway = function(direction){
   }
   game.encounter = false;
 }
-// using an item from the quickslots
+// using an item from the quickslots, both in and out of combat.
 var useItem = function(slot){
-  var effect = itemTable[player.inv[slot]].effect[0];
-  console.log(effect);
-  addToLog(itemTable[player.inv[slot]].fText);
-  switch (effect) {
-    case 'HPup':
-      player.pHp = player.pHp + itemTable[player.inv[slot]].effect[1];
-      player.inv[slot] = '';
-      break;
-    case 'Light':
+  if (player.inv[slot] == '') {
+    console.log('it was empty');
+    return;
+    }
+  else {
+    var effect = itemTable[player.inv[slot]].effect[0];
+    addToLog(itemTable[player.inv[slot]].fText);
+    switch (effect) {
+      case 'HPup':
+        player.pHp = player.pHp + itemTable[player.inv[slot]].effect[1];
         player.inv[slot] = '';
         break;
-    case 'read':
-        addToLog()
+      case 'Light':
+          player.inv[slot] = '';
+          break;
+      case 'read':
+          addToLog()
+          break;
+      case 'weapon':
+        var old = player.pWpn;
+        player.pWpn = player.inv[slot];
+        player.inv[slot] = old;
         break;
-    case 'weapon':
-      var old = player.pWpn;
-      player.pWpn = player.inv[slot];
-      player.inv[slot] = old;
-      break;
-    default:
+      default:
+    }
+    document.getElementById('Slot'+ slot).innerHTML = player.inv[slot];
   }
-  document.getElementById('Slot'+ slot).innerHTML = player.inv[slot];
 }
+
 // in case of combat
 var combat = function(action,option){
   switch (action) {
@@ -225,28 +260,7 @@ var combat = function(action,option){
     default:
   }
 }
-// in case of a non combat situation
-var noCombat = function(action,option){
-  switch (action) {
-    case 'move':
-      navigate(option)
-      break;
-    case 'search':
-      console.log(action + ' ' + 'Nothing yet');
-      break;
-    case 'action':
-      console.log(action + ' ' + 'Nothing here yet')
-      break;
-    case 'use':
-      useItem(option);
-      break;
-    case 'attack':
-      addToLog('You take a few practice swings with your' + ' ' + player.pWpn);
-      break;
-    default:
-    console.log('Did you press a broken button?');
-  }
-}
+
 // moving about
 var navigate = function(direction){
   switch (direction) {
@@ -263,13 +277,106 @@ var navigate = function(direction){
       addToLog('You walked west');
     break;
     default:
-      addToLog('This should never happen!!!')
+      addToLog('This should never happen!!!');
   }
   // maybe you get an encounter
   if (d10() > 9) {
-    encounterEvent()
+    encounterEvent();
   }
 }
+
+var lookAround = function(){
+  addToLog('You are in a' + ' ' + ground[0]);
+  addToLog('You see a:');
+  for (var i = 1; i < ground.length; i++) {
+    addToLog(ground[i]);
+  }
+}
+
+var dropItem = function(){
+  var slot = (prompt('What slot? (1 to 9)') - 1)
+  if (player.inv[slot] = '') {
+    addToLog('Nothing to drop')
+  }
+  else {
+    ground.push(player.inv[slot]);
+    player.inv[slot] = '';
+  }
+  refInv()
+}
+var takeItem = function(){
+  var item = prompt('What item?');
+  if (ground.indexOf(item) == -1){
+    addToLog('No such item found, check spelling and try again?');
+  }
+  else if (itemTable.index.indexOf(item) == -1) {
+    addToLog("You don't want that");
+  }
+  else {
+    var slot = (prompt('What slot do you want to put it in?') -1);
+    if (player.inv[slot] == '') {
+      player.inv[slot] = ground[ground.indexOf(item)];
+      ground[ground.indexOf(item)] = '';
+      ground = ground.filter(s => s.replace(/\s+/g, '').length !== 0);    // I ended up cheating. not my code. ask Tor Arne on tuesday about how this crap works.
+    }
+    else{addToLog('Slot is full, please choose an empty slot.')}
+  }
+  refInv();
+
+}
+
+
+var isactionopen = false;
+var actions = function(option){
+switch (option) {
+  case 'open':
+    if (!isactionopen) {
+          document.getElementById('actionmenu').classList.add('showactions');
+          isactionopen = true;
+    }
+    else {
+        document.getElementById('actionmenu').classList.remove('showactions');
+        isactionopen = false;
+    }
+    break;
+  case 'drop':
+    dropItem();
+    document.getElementById('actionmenu').classList.remove('showactions');
+    isactionopen = false;
+    break;
+  case 'take':
+      takeItem();
+      isactionopen = false;
+      document.getElementById('actionmenu').classList.remove('showactions');
+    break;
+  default:
+  console.log('Nuthing happened')
+  }
+}
+
+// in case of a non combat situation
+var noCombat = function(action,option){
+  switch (action) {
+    case 'move':
+      navigate(option)
+      break;
+    case 'search':
+      lookAround();
+      break;
+    case 'action':
+      actions(option);
+      break;
+    case 'use':
+      useItem(option);
+      break;
+    case 'attack':
+      addToLog('You take a few practice swings with your' + ' ' + player.pWpn);
+      break;
+    default:
+    console.log('Did you press a broken button?');
+  }
+}
+
 // this is the "main" loop of the script.
 function PlayerInput(action,option){
   if (!player.Status) {
